f8c02828205b4b8b6ad718be62bb88a3bd9d21c5
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-09-24 19:59:36 -0300

    feat: insert initial data for company and user tables

--- bd-script.sql
+++ bd-script.sql
@@ -181,3 +181,9 @@ CREATE TABLE IF NOT EXISTS contato (
     FOREIGN KEY (usuario_id)
         REFERENCES usuario (id)
 );
+
+INSERT INTO empresa (id, cnpj, nome_fantasia, razao_social) 
+VALUES (1, '11111111111111', 'Voltix Energy', 'Voltix Energy Solucoes em Energia LTDA');
+
+INSERT INTO usuario (id, nome, email, cargo, senha, empresa_id) 
+VALUES (1, 'Administrador', 'adm@voltix.com', 0, SHA2('teste123', 256), 1);
